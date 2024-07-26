a5c1fa987523dbbdb622c3f77c7209a1f2d3107d
KL_jiana <2436262516@qq.com>
2024-07-26 16:19:39 +0800

    fix crash, why it can run before?

--- src/main/java/com/teampotato/broken_blade/mixin/MixinRenderWroughtnaut.java
+++ src/main/java/com/teampotato/broken_blade/mixin/MixinRenderWroughtnaut.java
@@ -5,14 +5,17 @@ import com.teampotato.broken_blade.BrokenBlade;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Objects;
-
-@Mixin(value = RenderWroughtnaut.class, remap = false)
+@OnlyIn(Dist.CLIENT)
+@Mixin(value = RenderWroughtnaut.class)
 public abstract class MixinRenderWroughtnaut {
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;getDefaultInstance()Lnet/minecraft/world/item/ItemStack;"))
     private ItemStack getItemStack(Item instance) {
